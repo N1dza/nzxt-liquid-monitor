c6456b4974bd0a73a84cb17f65492fd0d433f790
Nikola <filicnikola2002@gmail.com>
2025-02-22 02:18:12 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,59 +1,39 @@
-// Ovaj kod inicijalizuje NZXT objekat i postavlja funkciju za ažuriranje podataka
+// NZXT API integracija za čitanje podataka o temperaturi
 window.nzxt = {
   v1: {
-    // Funkcija koja se poziva svaki put kada API ažurira podatke
     onMonitoringDataUpdate: function(data) {
-      // Logovanje celokupnog odgovora da bismo videli šta API vraća
-      console.log("API Data: ", data); // Ovdje ćemo proveriti strukturu podataka
+      // Izdvajanje podataka o tečnosti (kraken, liquids ili liq)
+      const { kraken, liquids, liq } = data;
 
-      // Izdvajamo sve relevantne podatke
-      const { cpus, gpus, ram, kraken, liquids, liq } = data;
-
-      // Logovanje specifičnih podataka
-      console.log("Kraken Data: ", kraken); // Podaci o Kraken-u
-      console.log("Liquid Data: ", liquids); // Podaci o Liquids
-      console.log("Liq Data: ", liq); // Podaci o Liq
-
-      // Pokreni debag za podatke o temperaturi
-      if (kraken) {
-        console.log("Kraken temperature: ", kraken.temperature); // Pokazivanje tačne temperature za Kraken
-      }
-      
-      if (liquids && liquids.length > 0) {
-        console.log("Liquids temperature: ", liquids[0].temperature); // Pokazivanje temperature za Liquid
-      }
-      
-      if (liq && liq.length > 0) {
-        console.log("Liq temperature: ", liq[0].temperature); // Pokazivanje temperature za Liq
-      }
-
-      // Ako imamo podatke o tečnosti, ažuriraj temperaturu
+      // Proveravamo koje podatke imamo
       let temperature = null;
 
-      // Ažuriranje temperature iz različitih izvora
+      // Ako imamo podatke o Kraken tečnosti
       if (kraken && kraken.temperature !== undefined) {
-        temperature = kraken.temperature; // Ako postoji, uzimamo temperaturu iz Kraken-a
-      } else if (liquids && liquids.length > 0 && liquids[0].temperature !== undefined) {
-        temperature = liquids[0].temperature; // Ako postoji, uzimamo temperaturu iz Liquids
-      } else if (liq && liq.length > 0 && liq[0].temperature !== undefined) {
-        temperature = liq[0].temperature; // Ako postoji, uzimamo temperaturu iz Liq
+        temperature = kraken.temperature;
+      }
+      // Ako imamo podatke o Liquids tečnosti
+      else if (liquids && liquids.length > 0 && liquids[0].temperature !== undefined) {
+        temperature = liquids[0].temperature;
+      }
+      // Ako imamo podatke o Liq tečnosti
+      else if (liq && liq.length > 0 && liq[0].temperature !== undefined) {
+        temperature = liq[0].temperature;
       }
 
-      // Ako smo našli temperaturu, ažuriramo prikaz
+      // Ako smo dobili temperaturu, ažuriraj je na stranici
       if (temperature !== null) {
-        updateTemperature(temperature); // Ažurira temperaturu
-      } else {
-        console.log("Nema podataka o tečnosti!");
+        updateTemperature(temperature);
       }
     }
   }
 }
 
-// Funkcija koja ažurira prikaz temperature na web stranici
+// Funkcija koja ažurira temperaturu na stranici
 function updateTemperature(temperature) {
   const temperatureElement = window.document.getElementById('temperature');
   
   if (temperatureElement) {
-    temperatureElement.innerText = temperature + "°C"; // Ažurira temperaturu u HTML-u
+    temperatureElement.innerText = temperature + "°C"; // Ažuriraj prikaz temperature
   }
 }
